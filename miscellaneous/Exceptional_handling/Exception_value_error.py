52e095388bd3b46838fd0290b57984d42b5f81fa
rajatisshere <106300950+rajatisshere@users.noreply.github.com>
2022-05-31 17:12:23 +0530

    Create Exception_value_error.py

---
+++ miscellaneous/Exceptional_handling/Exception_value_error.py
@@ -0,0 +1,19 @@
+try:
+
+  a=int(input("enter the value"))
+
+  b=int(input("enter the value"))
+
+  c=a/b
+
+  print("c=",c)
+
+except ZeroDivisionError:
+
+  print("denominator is 0")
+
+except ValueError:
+
+  print("unable to convert string to int")
+
+except:
